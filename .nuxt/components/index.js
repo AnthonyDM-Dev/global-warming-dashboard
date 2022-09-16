4540abae4958141baceac962a5da2cd2cdbd6c88
Anthony Dei Medici <adeimedi@everis.com>
2022-09-16 17:16:07 +0200

    testing

--- .nuxt/components/index.js
+++ .nuxt/components/index.js
@@ -1,17 +1,17 @@
-export { default as ActionButtons } from '../..\\components\\ActionButtons.vue'
-export { default as AirForm } from '../..\\components\\AirForm.vue'
-export { default as AlertPopup } from '../..\\components\\AlertPopup.vue'
-export { default as BannerCover } from '../..\\components\\BannerCover.vue'
-export { default as BarChart } from '../..\\components\\BarChart.vue'
-export { default as ChartLegend } from '../..\\components\\ChartLegend.vue'
-export { default as EarthModel } from '../..\\components\\EarthModel.vue'
-export { default as LineChart } from '../..\\components\\LineChart.vue'
-export { default as LoadingOverlay } from '../..\\components\\LoadingOverlay.vue'
-export { default as NavBar } from '../..\\components\\NavBar.vue'
-export { default as PropertyCard } from '../..\\components\\PropertyCard.vue'
-export { default as SpinnerLoader } from '../..\\components\\SpinnerLoader.vue'
-export { default as VideoHeader } from '../..\\components\\VideoHeader.vue'
-export { default as YoutubeVideo } from '../..\\components\\YoutubeVideo.vue'
+export const ActionButtons = () => import('../..\\components\\ActionButtons.vue' /* webpackChunkName: "components/action-buttons" */).then(c => wrapFunctional(c.default || c))
+export const AirForm = () => import('../..\\components\\AirForm.vue' /* webpackChunkName: "components/air-form" */).then(c => wrapFunctional(c.default || c))
+export const AlertPopup = () => import('../..\\components\\AlertPopup.vue' /* webpackChunkName: "components/alert-popup" */).then(c => wrapFunctional(c.default || c))
+export const BannerCover = () => import('../..\\components\\BannerCover.vue' /* webpackChunkName: "components/banner-cover" */).then(c => wrapFunctional(c.default || c))
+export const BarChart = () => import('../..\\components\\BarChart.vue' /* webpackChunkName: "components/bar-chart" */).then(c => wrapFunctional(c.default || c))
+export const ChartLegend = () => import('../..\\components\\ChartLegend.vue' /* webpackChunkName: "components/chart-legend" */).then(c => wrapFunctional(c.default || c))
+export const EarthModel = () => import('../..\\components\\EarthModel.vue' /* webpackChunkName: "components/earth-model" */).then(c => wrapFunctional(c.default || c))
+export const LineChart = () => import('../..\\components\\LineChart.vue' /* webpackChunkName: "components/line-chart" */).then(c => wrapFunctional(c.default || c))
+export const LoadingOverlay = () => import('../..\\components\\LoadingOverlay.vue' /* webpackChunkName: "components/loading-overlay" */).then(c => wrapFunctional(c.default || c))
+export const NavBar = () => import('../..\\components\\NavBar.vue' /* webpackChunkName: "components/nav-bar" */).then(c => wrapFunctional(c.default || c))
+export const PropertyCard = () => import('../..\\components\\PropertyCard.vue' /* webpackChunkName: "components/property-card" */).then(c => wrapFunctional(c.default || c))
+export const SpinnerLoader = () => import('../..\\components\\SpinnerLoader.vue' /* webpackChunkName: "components/spinner-loader" */).then(c => wrapFunctional(c.default || c))
+export const VideoHeader = () => import('../..\\components\\VideoHeader.vue' /* webpackChunkName: "components/video-header" */).then(c => wrapFunctional(c.default || c))
+export const YoutubeVideo = () => import('../..\\components\\YoutubeVideo.vue' /* webpackChunkName: "components/youtube-video" */).then(c => wrapFunctional(c.default || c))
 
 // nuxt/nuxt.js#8607
 function wrapFunctional(options) {
